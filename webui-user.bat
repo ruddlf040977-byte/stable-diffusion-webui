5df8ea4144b9b5c21bea2359e67d1cc4b20e53a3
ruddlf040977-byte <ruddlf040977@gmail.com>
2025-12-07 20:46:51 +0900

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,5 +3,5 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--skip-torch-cuda-test
+set COMMANDLINE_ARGS=--skip-torch-cuda-test --precision full --on-half --use-cpu all
 call webui.bat
